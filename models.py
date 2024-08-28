46cc274772db36f217e41d8284108398f9223bec
cbzZZZZ <1838355147@qq.com>
2024-08-28 18:23:36 +0800

    star/browse/api

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP,Text
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP,VARCHAR,Text
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 #1.用Base类来创建 SQLAlchemy 模型
 from database import Base
